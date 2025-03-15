3c7a14c604dd2c09a033c1da3984734292da3d06
Jarred Capellman <jcapellman@hotmail.com>
2025-03-15 16:20:57 -0400

    Added: Initial Commit of the Gore Engine Library

---
+++ GoreLib/GoreLib.cpp
@@ -0,0 +1,10 @@
+// GoreLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnGoreLib()
+{
+}
