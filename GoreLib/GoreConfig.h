40fc53c74bf37bf79974e2506209048218260f92
Jarred Capellman <jcapellman@hotmail.com>
2025-03-21 20:41:34 -0500

    Change: Refactored the GoreConfig class to a cpp file

--- GoreLib/GoreConfig.h
+++ GoreLib/GoreConfig.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "framework.h"
+
 constexpr auto DEFAULT_R_SCREEN_WIDTH = "720";
 constexpr auto DEFAULT_R_SCREEN_HEIGHT = "480";
 constexpr auto DEFAULT_R_BPP = "16";
@@ -16,85 +18,19 @@ enum GoreConfigKeys {
 
 class GoreConfig {
 public:
-	GoreConfig(const std::string& fileName = DEFAULT_CONFIG_FILENAME) {
-        std::ifstream file(fileName);
+    GoreConfig(const std::string& fileName = DEFAULT_CONFIG_FILENAME);
 
-        if (!file.is_open()) {
-            LoadDefaults();
+    std::string GetStringValue(const std::string& key) const;
 
-            return;
-            //throw std::runtime_error("Failed to open file: " + fileName);
-        }
+    int GetIntValue(const std::string& key) const;
 
-        std::string line;
-        while (std::getline(file, line)) {
-            auto delimiterPos = line.find('=');
+    std::string GetValue(GoreConfigKeys configKey);
 
-            if (delimiterPos == std::string::npos) {
-                throw std::runtime_error("Invalid format in file: " + line);
-            }
-            std::string key = line.substr(0, delimiterPos);
-            std::string value = line.substr(delimiterPos + 1);
+    int GetIntValue(GoreConfigKeys configKey);
 
-            _mapConfig[key] = value;
-        }
-
-        file.close();
-	}
-
-    std::string GetStringValue(const std::string& key) const {
-        auto it = _mapConfig.find(key);
-
-        if (it == _mapConfig.end()) {
-            throw std::runtime_error("Key not found: " + key);
-        }
-        return it->second;
-    }
-
-    int GetIntValue(const std::string& key) const {
-        std::string value = GetStringValue(key);
-
-        return std::stoi(value);
-    }
-
-    std::string GetValue(GoreConfigKeys configKey) {
-        std::string key = enum_to_string(configKey);
-
-        return GetStringValue(key);
-    }
-
-    int GetIntValue(GoreConfigKeys configKey) {
-        std::string key = enum_to_string(configKey);
-
-        return GetIntValue(key);
-    }
-
-    void LoadDefaults() {
-        _mapConfig[enum_to_string(GoreConfigKeys::R_SCREEN_WIDTH)] = DEFAULT_R_SCREEN_WIDTH;
-        _mapConfig[enum_to_string(GoreConfigKeys::R_SCREEN_HEIGHT)] = DEFAULT_R_SCREEN_WIDTH;
-        _mapConfig[enum_to_string(GoreConfigKeys::R_BPP)] = DEFAULT_R_BPP;
-        _mapConfig[enum_to_string(GoreConfigKeys::S_VOLUME)] = DEFAULT_S_VOLUME;
-        _mapConfig[enum_to_string(GoreConfigKeys::S_MUSIC_VOLUME)] = DEFAULT_S_MUSIC_VOLUME;
-    }
-
+    void LoadDefaults();
 private:
-    std::string enum_to_string(GoreConfigKeys configKey) {
-        static const std::map<GoreConfigKeys, std::string> enumToStringMap = {
-            {R_SCREEN_WIDTH, "R_SCREEN_WIDTH"},
-            {R_SCREEN_HEIGHT, "R_SCREEN_HEIGHT"},
-            {R_BPP, "R_BPP"},
-            {S_VOLUME, "S_VOLUME"},
-            {S_MUSIC_VOLUME, "S_MUSIC_VOLUME"}
-        };
+    std::string enum_to_string(GoreConfigKeys configKey);
 
-        auto it = enumToStringMap.find(configKey);
-
-        if (it != enumToStringMap.end()) {
-            return it->second;
-        }
-
-        throw std::runtime_error("Invalid GoreConfigKey");
-    }
-
-	std::map<std::string, std::string> _mapConfig;
+    std::map<std::string, std::string> _mapConfig;
 };
